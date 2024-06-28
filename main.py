bafc2d5a5dbe6aceb965fc63414470a8eceeeb3e
rie40 <rie4015@yandex.ru>
2024-06-28 22:39:23 +0500

    gendataset

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import sys
 import time
 import numpy as np
 import pybullet as p
 import pybullet_data
 import pandas as pd
-
+import time
 
 air_friction_coefficient = 0.1
 
@@ -18,7 +19,7 @@ def apply_air_friction(object_id, linear_velocity):
 PB_BallMass = 0.450  # масса шара
 PB_BallRadius = 0.35  # радиус шара
 # Инициализация PyBullet
-physicsClient = p.connect(p.GUI, options=' --mp4=moviename.mp4')# запись видео
+physicsClient = p.connect(p.DIRECT)# запись видео
 # Установка параметров камеры
 cameraDistance = 30
 cameraYaw = 0
@@ -35,6 +36,7 @@ p.setGravity(0, 0, -9.81)
 
 # Загрузка модели
 
+
 class Ball():
     def __init__(self,ballPosition, ballOrientation):
         #ballPosition = [0, 0, 2]
@@ -43,8 +45,10 @@ class Ball():
         self.ballColShape = p.createCollisionShape(p.GEOM_SPHERE, radius=PB_BallRadius)
 
         self.ballVisualShapeId = p.createVisualShape(p.GEOM_SPHERE, radius=PB_BallRadius, rgbaColor=[0.25, 0.75, 0.25, 1])
-        self.p_ballId = p.createMultiBody(PB_BallMass, self.ballColShape, self.ballVisualShapeId, ballPosition,
-                                                    ballOrientation)  # (mass, collisionShape, visualShape, ballPosition, ballOrientation)
+        self.p_ballId = p.createMultiBody(PB_BallMass, self.ballColShape, self.ballVisualShapeId,
+                                          basePosition = ballPosition,
+                                                    baseOrientation = ballOrientation)
+        # (mass, collisionShape, visualShape, ballPosition, ballOrientation)
         p.changeVisualShape(self.p_ballId,-1,rgbaColor=[1,0.27,0,1])
 
 
@@ -54,9 +58,15 @@ class Ball():
 #ball = Ball([0, 0, 2],[0, 0, 0, 1])
 # Подготовка DataFrame для записи данных
 df = pd.DataFrame(columns=['Throw_ID','time_step', 'pos_x', 'pos_y', 'pos_z'])
-
+steps = 10000
 # Цикл симуляции и запись данных
-for i in range(60):
+start = time.time()
+end = time.time() - start
+for i in range(steps):
+    print(i, '/', steps, end)
+    start = time.time()
+    # print(sys.getsizeof(df))
+
     p.resetSimulation()
     p.setGravity(0, 0, -9.81)
     # Создание формы коллизии для поверхности (например, плоскости)
@@ -65,24 +75,29 @@ for i in range(60):
     plane_body = p.createMultiBody(baseCollisionShapeIndex=plane_shape, basePosition=[0, 0, 0])
     p.changeDynamics(plane_body, -1, restitution=restitution)
     p.changeDynamics(plane_body, -1, lateralFriction=friction)
+    ballpos = np.random.uniform(-10,10,4)
+    ballpos[2]= PB_BallRadius + 0.01
+    ballorin = ballpos
+    ballorin[3] = 0
 
-    ball = Ball([0, 0, 5],[0, 0, 0, 5])
+    ball = Ball(ballpos[0:3], ballorin)
     p.changeDynamics(ball.p_ballId, -1, restitution=restitution)
     p.changeDynamics(ball.p_ballId, -1, lateralFriction=friction)
-    p_xforce = np.random.uniform(500,1500,1)
-    p_yforce = np.random.uniform(500,1500,1)
-    p_zforce = np.random.uniform(500,1500,1)
-    p.applyExternalForce(ball.p_ballId, -1, [p_xforce, p_yforce, p_zforce], [0, 0, 0], p.LINK_FRAME)
+    p_xforce = np.random.uniform(500,10000,1)
+    p_yforce = np.random.uniform(500,10000,1)
+    p_zforce = np.random.uniform(5000,15000,1)
+    p_xforce[0], p_yforce[0],p_zforce[0] = 1000,1000,10000
+    p.applyExternalForce(ball.p_ballId, -1, [p_xforce[0], p_yforce[0], p_zforce[0]], [0,0,0], p.LINK_FRAME)
     pos, _ = p.getBasePositionAndOrientation(ball.p_ballId)
     j = 0
-    while pos[2] > PB_BallRadius:
+    for j in range(1024):
         p.stepSimulation()
         linearVelocity, _ = p.getBaseVelocity(ball.p_ballId)
         apply_air_friction(ball.p_ballId, linearVelocity)
         pos, _ = p.getBasePositionAndOrientation(ball.p_ballId)
         df = df._append({'Throw_ID': i,'time_step': j, 'pos_x': pos[0], 'pos_y': pos[1], 'pos_z': pos[2]}, ignore_index=True)
-        #time.sleep(1. / 240.)
-        j+=1
+        # time.sleep(1. / 240.)
+    end = time.time() - start
 
 # Сохраняем датасет
 df.to_csv('simulation_dataset.csv', index=False)
