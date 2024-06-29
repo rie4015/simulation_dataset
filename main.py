8e2b1c2be8c22714fee84a03e3a1cdfe56885bb5
rie40 <rie4015@yandex.ru>
2024-06-29 23:47:26 +0500

    1/30 64

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def apply_air_friction(object_id, linear_velocity):
 PB_BallMass = 0.450  # масса шара
 PB_BallRadius = 0.35  # радиус шара
 # Инициализация PyBullet
-physicsClient = p.connect(p.DIRECT)# запись видео
+physicsClient = p.connect(p.DIRECT)# запись видео , options='--mp4=moviename.mp4'
 # Установка параметров камеры
 cameraDistance = 30
 cameraYaw = 0
@@ -54,12 +54,12 @@ class Ball():
 
 
 
-p.setTimeStep(1/30)
+#p.setTimeStep(1/30)
 #ball = Ball([0, 0, 2],[0, 0, 0, 1])
 # Подготовка DataFrame для записи данных
 df = pd.DataFrame(columns=['Throw_ID','time_step', 'pos_x', 'pos_y', 'pos_z'])
 df.to_csv('simulation_dataset.csv', index=False)
-steps = 5000
+steps = 300
 # Цикл симуляции и запись данных
 start = time.time()
 end = time.time() - start
@@ -83,13 +83,13 @@ for i in range(steps):
     ball = Ball(ballpos[0:3], ballorin)
     p.changeDynamics(ball.p_ballId, -1, restitution=restitution)
     p.changeDynamics(ball.p_ballId, -1, lateralFriction=friction)
-    p_xforce = np.random.uniform(500,10000,1)
-    p_yforce = np.random.uniform(500,10000,1)
-    p_zforce = np.random.uniform(5000,15000,1)
+    p_xforce = np.random.uniform(1500,10000,1)
+    p_yforce = np.random.uniform(1500,10000,1)
+    p_zforce = np.random.uniform(7000,15000,1)
     p_xforce[0], p_yforce[0],p_zforce[0] = 1000,1000,10000
     p.applyExternalForce(ball.p_ballId, -1, [p_xforce[0], p_yforce[0], p_zforce[0]], [0,0,0], p.LINK_FRAME)
     pos, _ = p.getBasePositionAndOrientation(ball.p_ballId)
-    for j in range(64):
+    for j in range(240 * 4):
         p.stepSimulation()
         linearVelocity, _ = p.getBaseVelocity(ball.p_ballId)
         apply_air_friction(ball.p_ballId, linearVelocity)
